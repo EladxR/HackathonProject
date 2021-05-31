dc05b3d347a1b340c0c48e97874ed0e47f580b92
Elad <eladridel@gmail.com>
2021-05-31 20:53:20 +0300

    3

--- app/src/main/java/com/example/hackthonproject/AddCourses.java
+++ app/src/main/java/com/example/hackthonproject/AddCourses.java
@@ -8,22 +8,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class AddCourses extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser user;
     DatabaseReference rootRef;
-    EditText courseText;
+    String course;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_courses);
-        courseText=findViewById(R.id.editTextAddCourse);
+        course="";
+
+        MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.spinnerChooseCourse);
+        spinner.setItems("Calculus", "Linear algebra ", "Algorithms");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                //Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+                course=item;
+            }
+        });
+
 
     }
 
@@ -38,10 +51,13 @@ public class AddCourses extends AppCompatActivity {
     }
 
     public void onAddCourse(View v){
-        String course= String.valueOf(courseText.getText());
-        rootRef.child("Users").child(user.getUid()).child("course").setValue(course);
-        Toast.makeText(AddCourses.this, course,Toast.LENGTH_LONG).show();
-
+        //String course= String.valueOf(courseText.getText());
+        if(course.equals("")){
+            Toast.makeText(AddCourses.this, "please choose course first", Toast.LENGTH_LONG).show();
+        }else {
+            rootRef.child("Users").child(user.getUid()).child("course").setValue(course);
+            Toast.makeText(AddCourses.this, course, Toast.LENGTH_LONG).show();
+        }
 
     }
 
