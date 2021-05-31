7261279940321b31436302860401d9c30b1cb765
Elad <eladridel@gmail.com>
2021-05-31 22:49:26 +0300

    fragmentss

--- app/src/main/java/com/example/hackthonproject/WelcomeActivity.java
+++ app/src/main/java/com/example/hackthonproject/WelcomeActivity.java
@@ -2,45 +2,41 @@ package com.example.hackthonproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+
+        Log.d("Debug","gggggggggggggggggggggggg");
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.teal_200)
+                .backgroundDark(R.color.teal_700)
+                .fragment(R.layout.fragment_first, R.style.Theme_Design_Light)
+                .build());
+        getSupportFragmentManager().beginTransaction()
+                .add(android.R.id.content, new Add_Courses().commit());
+
+        Log.d("Debug","hhhhhhhhhhhhhhhhhhhhhhhhh");
     }
 
-    public void onContinue(View view) {
-        EditText inputUsername=findViewById(R.id.inputUsernameWelcome);
-        String username=inputUsername.getText().toString();
-        if(TextUtils.isEmpty(username)){
-            Toast.makeText(this,"please enter username",Toast.LENGTH_LONG).show();
-        }else{
-            SaveUsername(username);
-            SendToMainActivity();
-        }
 
-    }
-
-    private void SendToMainActivity() {
-        Intent mainIntent = new Intent(this, MainActivity.class);
-        startActivity(mainIntent);
-        finish();
-    }
-
-    private void SaveUsername(String username) {
-        String userID= FirebaseAuth.getInstance().getCurrentUser().getUid(); // shouldn't be null
-        //set username
-        FirebaseDatabase.getInstance().getReference().child("Users").child(userID).child("username").setValue(username);
-    }
 }
