169db77377f6c452ef9afe18949c1acf8c028ef7
Gal <galuziel3@gmail.com>
2021-05-31 20:27:35 +0300

    gal's test

---
+++ app/src/main/java/com/example/hackthonproject/LoginActivity.java
@@ -0,0 +1,120 @@
+package com.example.hackthonproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.iid.FirebaseInstanceId;
+//import com.google.firebase.iid.InstanceIdResult;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+    private EditText userEmail,userPassword;
+
+    private ProgressDialog loadingBar;
+
+    public static Activity LastLoginActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        userEmail=findViewById(R.id.inputEmail);
+        userPassword=findViewById(R.id.inputPassword);
+
+        mAuth=FirebaseAuth.getInstance();
+        user=mAuth.getCurrentUser();
+
+        loadingBar=new ProgressDialog(this);
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+        LastLoginActivity=this;
+
+        if(user!=null){
+            ToMainActivity();
+            finish();
+        }
+
+    }
+
+    private void ToMainActivity() {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLogin(View v){
+        String email=userEmail.getText().toString();
+        String password=userPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"enter email..",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"enter password..",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //no empty fields
+        loadingBar.setTitle("Logging in..");
+        loadingBar.setCanceledOnTouchOutside(true);
+        loadingBar.show();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                loadingBar.dismiss();
+                if(task.isSuccessful()){
+                    //SaveToken();
+                    ToMainActivity();
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this,"Error: "+task.getException().toString(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+    /*private void SaveToken() {
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(task.isSuccessful()) {
+                    String deviceToken = task.getResult().getToken();
+                    user=mAuth.getCurrentUser();
+                    DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+                    userRef.child("deviceToken").setValue(deviceToken);
+                }else{
+                    Toast.makeText(LoginActivity.this,"error save token",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }*/
+
+    public void onToRegister(View v){
+        Intent intent=new Intent(this,SignupActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hackthonproject/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.hackthonproject;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.hackthonproject.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseUser user;
+    public static String username; // changes on data change
+    public static String profileImage=null;
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loadingBar;
+    private boolean welcomeOnlyOnce=false;
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadingBar=new ProgressDialog(this);
+        loadingBar.setTitle("Logging in..");
+        loadingBar.setCanceledOnTouchOutside(true);
+        loadingBar.show();
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth=FirebaseAuth.getInstance();
+
+        user=mAuth.getCurrentUser();
+
+        if(user==null){
+            ToLoginActivity();
+            loadingBar.dismiss();
+            finish();
+        }else{
+            //first get data
+
+            FirebaseDatabase.getInstance("https://hackthonproject-1d1d6-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Users").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    username= String.valueOf(dataSnapshot.child("username").getValue());
+                    if(dataSnapshot.child("firstTime").exists() && String.valueOf(dataSnapshot.child("firstTime").getValue()).equals("no")) {
+                        if(!welcomeOnlyOnce)
+                            //welcome toast
+                            Toast.makeText(MainActivity.this, "Welcome " + username, Toast.LENGTH_LONG).show();
+                        welcomeOnlyOnce=true;
+
+                    }else{ // no user name init-> to welcome activity
+                        SendToWelcomeActivity();
+                        loadingBar.dismiss();
+                        finish(); // so user will not be able to go back here
+                    }
+                    if(dataSnapshot.child("profileImage").exists()){
+                        profileImage=String.valueOf(dataSnapshot.child("profileImage").getValue());
+                    }
+
+                    // finished loading
+                    loadingBar.dismiss();
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+    }
+
+    private void SendToWelcomeActivity() {
+        Intent intent=new Intent(this,AddCourses.class);
+        startActivity(intent);
+    }
+
+    private void ToLoginActivity() {
+        Intent intent=new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId()==R.id.settingsOption){
+
+        }
+        if(item.getItemId()==R.id.signOutOption){
+            mAuth.signOut();
+            ToLoginActivity();
+        }
+        if(item.getItemId()==R.id.editProfileOption){
+            //ToEditProfileActivity();
+        }
+
+        return true;
+    }
+
+}
